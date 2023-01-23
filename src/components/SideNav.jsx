a1b9e1d4f9a994cdce8f8fbabd87857ff16027b8
Mona Lisa <athman.abu@gmail.com>
2023-01-23 10:12:24 +0300

    side bar

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -4,6 +4,12 @@ import {
   HiOutlineCube,
   HiOutlineDesktopComputer,
 } from "react-icons/hi";
+import {
+  RiLinkedinFill,
+  RiTwitterFill,
+  RiYoutubeFill,
+  RiGithubFill,
+} from "react-icons/ri";
 function SideNav() {
   return (
     <div className="fixed top-0 left-0 bg-gray-800 h-screen w-20 flex-none py-6">
@@ -12,17 +18,17 @@ function SideNav() {
         logo
       </div>
       {/* nav */}
-      <nav className="flex flex-col justify-center items-center space-y-6">
+      <nav className="flex flex-col justify-center items-center space-y-2">
         {/* home */}
         <a
           href="#"
           className="text-white block p-4 px-auto bg-gray-700 rounded-full hover:bg-yellowAccent hover:rounded-3xl transition-all ease-in-out group relative"
         >
           <HiOutlineHome
-            size={30}
+            size={24}
             className="text-yellowAccent group-hover:text-white"
           />
-          <span class="absolute flex-row w-auto p-2 m-2 px-4 rounded-md left-16 bottom-1 shadow-md text-gray-100 bg-blackBg font-semibold transition-all duration-100 scale-0 origin-left group-hover:scale-100">
+          <span className="absolute flex-row w-auto p-2 m-2 px-4 rounded-md left-16 bottom-1 shadow-md text-gray-100 bg-blackBg font-semibold transition-all duration-100 scale-0 origin-left group-hover:scale-100">
             Home
           </span>
         </a>
@@ -32,10 +38,10 @@ function SideNav() {
           className="text-white block p-4 px-auto bg-gray-700 rounded-full hover:bg-yellowAccent hover:rounded-3xl transition-all ease-in-out group relative"
         >
           <HiOutlineUser
-            size={30}
+            size={24}
             className="text-yellowAccent group-hover:text-white"
           />
-          <span class="absolute flex-row w-auto p-2 m-2 px-4 rounded-md left-16 bottom-1 shadow-md text-gray-100 bg-blackBg font-semibold transition-all duration-100 scale-0 origin-left group-hover:scale-100">
+          <span className="absolute flex-row w-auto p-2 m-2 px-4 rounded-md left-16 bottom-1 shadow-md text-gray-100 bg-blackBg font-semibold transition-all duration-100 scale-0 origin-left group-hover:scale-100">
             About
           </span>
         </a>
@@ -45,10 +51,10 @@ function SideNav() {
           className="text-white block p-4 px-auto bg-gray-700 rounded-full hover:bg-yellowAccent hover:rounded-3xl transition-all ease-in-out group relative"
         >
           <HiOutlineCube
-            size={30}
+            size={24}
             className="text-yellowAccent group-hover:text-white"
           />
-          <span class="absolute flex-row w-auto p-2 m-2 px-4 rounded-md left-16 bottom-1 shadow-md text-gray-100 bg-blackBg font-semibold transition-all duration-100 scale-0 origin-left group-hover:scale-100">
+          <span className="absolute flex-row w-auto p-2 m-2 px-4 rounded-md left-16 bottom-1 shadow-md text-gray-100 bg-blackBg font-semibold transition-all duration-100 scale-0 origin-left group-hover:scale-100">
             Projects
           </span>
         </a>
@@ -58,15 +64,77 @@ function SideNav() {
           className="text-white block p-4 px-auto bg-gray-700 rounded-full hover:bg-yellowAccent hover:rounded-3xl transition-all ease-in-out group relative"
         >
           <HiOutlineDesktopComputer
-            size={30}
+            size={24}
             className="text-yellowAccent group-hover:text-white"
           />
-          <span class="absolute flex-row w-auto p-2 m-2 px-4 rounded-md left-16 bottom-1 shadow-md text-gray-100 bg-blackBg font-semibold transition-all duration-100 scale-0 origin-left group-hover:scale-100">
+          <span className="absolute flex-row w-auto p-2 m-2 px-4 rounded-md left-16 bottom-1 shadow-md text-gray-100 bg-blackBg font-semibold transition-all duration-100 scale-0 origin-left group-hover:scale-100">
             Workflow
           </span>
         </a>
-        {/* clients */}
       </nav>
+      {/* socials */}
+      <div className="flex flex-col justify-center items-center space-y-2 mt-8">
+        {/* YouTube */}
+        <a
+          href="https://www.youtube.com/@programwithabu"
+          target="_blank"
+          rel="noreferrer"
+          className="text-white block p-4 px-auto bg-gray-700 rounded-full hover:bg-yellowAccent hover:rounded-3xl transition-all ease-in-out group relative"
+        >
+          <RiYoutubeFill
+            size={24}
+            className="text-yellowAccent group-hover:text-white"
+          />
+          <span className="absolute flex-row w-auto p-2 m-2 px-4 rounded-md left-16 bottom-1 shadow-md text-gray-100 bg-blackBg font-semibold transition-all duration-100 scale-0 origin-left group-hover:scale-100">
+            YouTube
+          </span>
+        </a>
+        {/* Linkedin */}
+        <a
+          href="https://www.linkedin.com/in/abubakar-athman/"
+          target="_blank"
+          rel="noreferrer"
+          className="text-white block p-4 px-auto bg-gray-700 rounded-full hover:bg-yellowAccent hover:rounded-3xl transition-all ease-in-out group relative"
+        >
+          <RiLinkedinFill
+            size={24}
+            className="text-yellowAccent group-hover:text-white"
+          />
+          <span className="absolute flex-row w-auto p-2 m-2 px-4 rounded-md left-16 bottom-1 shadow-md text-gray-100 bg-blackBg font-semibold transition-all duration-100 scale-0 origin-left group-hover:scale-100">
+            Linkedin
+          </span>
+        </a>
+        {/* Github */}
+        <a
+          href="https://github.com/iAthman83"
+          target="_blank"
+          rel="noreferrer"
+          className="text-white block p-4 px-auto bg-gray-700 rounded-full hover:bg-yellowAccent hover:rounded-3xl transition-all ease-in-out group relative"
+        >
+          <RiGithubFill
+            size={24}
+            className="text-yellowAccent group-hover:text-white"
+          />
+          <span className="absolute flex-row w-auto p-2 m-2 px-4 rounded-md left-16 bottom-1 shadow-md text-gray-100 bg-blackBg font-semibold transition-all duration-100 scale-0 origin-left group-hover:scale-100">
+            Github
+          </span>
+        </a>
+        {/* Twitter */}
+        <a
+          href="https://twitter.com/iathman83"
+          target="_blank"
+          rel="noreferrer"
+          className="text-white block p-4 px-auto bg-gray-700 rounded-full hover:bg-yellowAccent hover:rounded-3xl transition-all ease-in-out group relative"
+        >
+          <RiTwitterFill
+            size={24}
+            className="text-yellowAccent group-hover:text-white"
+          />
+          <span className="absolute flex-row w-auto p-2 m-2 px-4 rounded-md left-16 bottom-1 shadow-md text-gray-100 bg-blackBg font-semibold transition-all duration-100 scale-0 origin-left group-hover:scale-100">
+            Twitter
+          </span>
+        </a>
+      </div>
     </div>
   );
 }

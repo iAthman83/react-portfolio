06a51d9aea40e1d3f7d67c5535c5033491e1e361
Mona Lisa <athman.abu@gmail.com>
2023-01-25 09:16:33 +0300

    create projects card

--- src/sections/ProjectsSection.jsx
+++ src/sections/ProjectsSection.jsx
@@ -1,17 +1,24 @@
 import { forwardRef } from "react";
+import ProjectsCardComponent from "../components/ProjectsCardComponent";
 
 const ProjectsSection = forwardRef((props, ref) => {
   return (
-    <div ref={ref} className="container mx-auto h-screen flex flex-col">
+    <div ref={ref} className="container mx-auto flex flex-col space-y-20 my-8">
       <div className="flex flex-col items-center justify-center space-y-4">
         <h1 className="text-5xl text-sideBarText font-sofia font-extrabold">
           PROJECTS
         </h1>
-        <p className="text-base mb-12">
+        <p className="text-base mb-18">
           Have a look at some of my projects below, ranging from React to Vue
           and WordPress
         </p>
-        <div className="border-b-4 w-32"></div>
+        <div className="border-b-8 border-zinc-400 w-28 text-sideBarBg">.</div>
+      </div>
+      <div className="flex flex-row flex-wrap space-x-4 items-center justify-center">
+        <ProjectsCardComponent />
+        <ProjectsCardComponent />
+        <ProjectsCardComponent />
+        <ProjectsCardComponent />
       </div>
     </div>
   );

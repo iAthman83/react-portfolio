6e689a68e09b695a7b2cca0d0c402478fe9ce682
Mona Lisa <athman.abu@gmail.com>
2023-02-01 17:49:00 +0300

    about me section

--- src/sections/AboutSection.jsx
+++ src/sections/AboutSection.jsx
@@ -1,9 +1,10 @@
 import { forwardRef } from "react";
 import YouTube from "react-youtube";
+import { RiYoutubeFill, RiMailFill } from "react-icons/ri";
 
 const AboutSection = forwardRef((props, ref) => {
   const opts = {
-    height: "390",
+    height: "360",
     width: "640",
     playerVars: {
       // https://developers.google.com/youtube/player_parameters
@@ -11,7 +12,7 @@ const AboutSection = forwardRef((props, ref) => {
     },
   };
   return (
-    <div ref={ref} className="container mx-auto h-screen bg-grayBg py-8">
+    <div ref={ref} className="container mx-auto bg-grayBg py-20">
       <div className="flex flex-col items-center justify-center space-y-4">
         <h1 className="text-5xl text-sideBarText font-sofia font-extrabold">
           ABOUT ME
@@ -36,12 +37,39 @@ const AboutSection = forwardRef((props, ref) => {
           <p className="text-textGray text-lg">
             My love for technology and programming drives me to continuously
             learn and stay up to date with the latest industry trends and
-            developments. I am excited to bring my technical expertise and
-            problem-solving skills to new projects and opportunities.
+            developments. I also like to share my knowledge through video
+            tutorials on my YouTube channel.
           </p>
+          <div className="flex space-x-4">
+            <a
+              href="mailto:abuathman83@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+              className="flex space-x-2 items-center cursor-pointer text-white p-2 px-auto bg-emerald-500 px-8 rounded-lg hover:bg-emerald-600"
+            >
+              {/* <RiMailFill size={25} className="text-mailBlue" /> */}
+              <h1 className="text-lg">Work with me</h1>
+            </a>
+            <a
+              href="https://www.youtube.com/@programwithabu"
+              target="_blank"
+              rel="noreferrer"
+              className="flex space-x-2 items-center cursor-pointer text-white p-2 px-auto bg-sideBarHighlight px-14 rounded-lg hover:bg-zinc-700"
+            >
+              {/* <RiYoutubeFill size={25} className="text-youtubeRed" /> */}
+              <h1 className="text-lg">Subscribe</h1>
+            </a>
+          </div>
         </div>
-        <div className="flex flex-1 rounded-lg overflow-hidden">
-          <YouTube videoId="_G5RxzxJz4A" opts={opts} />
+        <div className="flex flex-1 flex-col space-y-8">
+          <h1 className="text-sideBarText text-2xl font-sofia font-extrabold">
+            One of my many tutorials
+          </h1>
+          <YouTube
+            className="rounded-lg overflow-hidden"
+            videoId="_G5RxzxJz4A"
+            opts={opts}
+          />
         </div>
       </div>
     </div>

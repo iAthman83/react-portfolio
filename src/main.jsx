e3db0636a3fd7a7821066c9e671f0da4077e2886
iAthman83 <athman.abu@.com>
2023-07-25 13:22:39 +0300

    fix vercel error

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import BlogPage from "./pages/blogPage";
+import Blog from "./pages/Blog";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/blog",
-    element: <BlogPage />,
+    element: <Blog />,
   },
   {
     path: "*",
--- src/pages/BlogPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const BlogPage = () => {
-  return <div>my blog page</div>;
-};
-
-export default BlogPage;

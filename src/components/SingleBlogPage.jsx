d2bd7944c803c79825490149043896f68456295e
iAthman83 <athman.abu@.com>
2023-07-26 14:40:12 +0300

    single blog post page

---
+++ src/components/SingleBlogPage.jsx
@@ -0,0 +1,72 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { PortableText } from "@portabletext/react";
+import { client } from "../../lib/client";
+
+const SingleBlogPage = () => {
+  const params = useParams();
+
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    client
+      .fetch(
+        `*[_type == 'post']{
+    _id,
+    title,
+    "slug": slug.current,
+    "image": mainImage.asset->url,
+    publishedAt,
+    "body": pt::text(body),
+    "author": *[_type == 'author' && _id == ^.author._ref][0]{
+      _id,
+      name,
+      "slug": slug.current,
+      },
+      categories[]->{
+      _id,
+      title
+    },
+    "content": body,
+  }`
+      )
+      .then((data) => setPosts(data))
+      .catch(console.error);
+  }, []);
+
+  if (posts.length < 0) return <div>Loading...</div>;
+  else if (posts.length > 0) {
+    const singlePost = posts.find((post) => post._id === params.postId);
+
+    console.log(singlePost);
+    return (
+      <div className="flex space-y-8 my-8 flex-col lg:px-40 px-4 min-h-screen">
+        <div className="w-full flex flex-col justify-center space-y-8 items-center">
+          <h1 className="text-2xl text-primary-dark font-semibold drop-shadow-md">
+            {singlePost.title}
+          </h1>
+          <img
+            src={singlePost.image}
+            width={600}
+            height={200}
+            alt={singlePost.title}
+            className="backdrop-brightness-100"
+          />
+        </div>
+        <div>
+          <p>
+            By{" "}
+            <span className="font-semibold text-primary-brand">
+              {singlePost.author.name},{" "}
+              {Date(singlePost.publishedAt).substring(0, 10)}
+            </span>
+          </p>
+        </div>
+        <div>
+          <PortableText value={singlePost.content} />
+        </div>
+      </div>
+    );
+  }
+};
+
+export default SingleBlogPage;
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Blog from "./pages/Blog";
+import SingleBlogPage from "./components/SingleBlogPage";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: "/blog",
     element: <Blog />,
+  },
+  {
+    path: "/blog/:postId",
+    element: <SingleBlogPage />,
   },
   {
     path: "*",

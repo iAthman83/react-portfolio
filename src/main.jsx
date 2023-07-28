7d2b29ca95026b283ff67ca441dcab108584cc9a
iAthman83 <athman.abu@.com>
2023-07-28 15:32:07 +0300

    refactor code

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Blog from "./pages/Blog";
-import SingleBlogPage from "./components/SingleBlogPage";
+import SingleBlogPage from "./pages/SingleBlogPage";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
